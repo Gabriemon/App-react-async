5d7dbfeeead8bf7f2a2c96b873b88b71e501ca78
Your Name <you@example.com>
2024-05-09 22:30:21 -0300

    Desenvolvimento

---
+++ App.tsx
@@ -0,0 +1,30 @@
+import { PaperProvider } from "react-native-paper";
+import { NavigationContainer } from "@react-navigation/native";
+import { RootStackParamList } from "./src/types/AppTypes";
+import { createStackNavigator } from "@react-navigation/stack";
+import HomeScreen from "./src/screens/HomeScreen";
+import AddNotesScreen from "./src/screens/AddNotesScreen";
+
+const Stack = createStackNavigator<RootStackParamList>();
+
+export default function App() {
+  return (
+    <PaperProvider>
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          <Stack.Screen 
+              name="Home" 
+              component={HomeScreen} 
+              options={{title: 'Lista de Notas'}}
+          />
+          <Stack.Screen
+              name="AddNote"
+              component={AddNotesScreen}
+              options={{title: 'Adicionar Nota'}}
+              initialParams={{ onAddNote: () => {} }}
+          />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </PaperProvider>    
+  );
+}
